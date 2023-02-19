fbfaec98b99efbb19bffd8bf2d3213298ed49bdc
zhangwy324 <zhangwy@ad.unc.edu>
2023-02-19 18:40:29 -0500

    finished editing rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,83 @@
 #!/usr/bin/env node
+import minimist from 'minimist'
+import {rpsls} from '../lib/rpsls.js'
+
+const args = minimist(process.argv.slice(2));
+
+if(args.h || args.help){
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+    process.exit(0);
+}
+
+if(args.r || args.rules){
+    console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+
+if(args._.length > 1){
+    console.log('[ARGUMENT] is out of range.');
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+                      console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+                      - Scissors CUTS Paper
+                      - Paper COVERS Rock
+                      - Rock SMOOSHES Lizard
+                      - Lizard POISONS Spock
+                      - Spock SMASHES Scissors
+                      - Scissors DECAPITATES Lizard
+                      - Lizard EATS Paper
+                      - Paper DISPROVES Spock
+                      - Spock VAPORIZES Rock
+                      - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+
+if(args._.length === 0){
+    console.log(JSON.stringify(rpsls()));
+    process.exit(0);
+}
+
+if(args._.length === 1){
+
+    const playerChoice = args._[0].toString().toLowerCase();
+    const rpslsArr = [ 'rock','paper','scissors','lizard','spock'];
+    if(rpslsArr.includes(playerChoice)){
+        console.log(JSON.stringify(rpsls(playerChoice)));
+        process.exit(0);
+    } else {
+        console.log('Invalid argument');
+        console.log(`Available options: rock, paper, scissors, lizard, spock`);
+        process.exit(0);
+    }
+}
